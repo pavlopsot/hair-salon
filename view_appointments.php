af7d11117a7fa9e520d021aad6b15b1c4af31403
pavlopsot <pavlopsot@gmail.com>
2025-03-19 11:36:45 +0200

    Update view_appointments.php

--- view_appointments.php
+++ view_appointments.php
@@ -15,9 +15,15 @@ $result = $conn->query($sql);
 
 echo "<h2>Προγραμματισμένα Ραντεβού</h2>";
 if ($result->num_rows > 0) {
-    echo "<table border='1'><tr><th>Όνομα</th><th>Τηλέφωνο</th><th>Ημερομηνία</th><th>Ώρα</th></tr>";
+    echo "<table border='1'><tr><th>Όνομα</th><th>Τηλέφωνο</th><th>Ημερομηνία</th><th>Ώρα</th><th>Ενέργεια</th></tr>";
     while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["name"]."</td><td>".$row["phone"]."</td><td>".$row["date"]."</td><td>".$row["time"]."</td></tr>";
+        echo "<tr>
+                <td>".$row["name"]."</td>
+                <td>".$row["phone"]."</td>
+                <td>".$row["date"]."</td>
+                <td>".$row["time"]."</td>
+                <td><a href='delete_appointment.php?id=".$row["id"]."' onclick='return confirm(\"Είσαι σίγουρος ότι θέλεις να ακυρώσεις αυτό το ραντεβού;\")'>Διαγραφή</a></td>
+              </tr>";
     }
     echo "</table>";
 } else {
